616c151e4de56917c65a9248d7275267bf4e8dcf
David Kucsai <david.kucsai@gmail.com>
2019-06-29 13:28:48 +0800

    Update version

--- lib/src/Table.d.ts
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-interface TableProps {
-    data?: any[];
-}
-export declare class Table extends React.PureComponent<TableProps> {
-    render(): JSX.Element;
-}
-export {};
--- lib/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from "./DataTableCell";
-export * from "./Table";
-export * from "./TableBody";
-export * from "./TableCell";
-export * from "./TableHeader";
-export * from "./TableRow";
-export * from "./Utils";
